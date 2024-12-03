4068f3610f02a288adb6defcfa73b4bf1bcfd645
claireandj666 <claire.yandi.chen@gmail.com>
2024-12-03 10:01:26 -0800

    created schemas for user, project, and page

---
+++ server/models/userModel.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	githubId: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    profileUrl: String,
+    avatarUrl: String,
+    projects: [{ type : Schema.Types.ObjectId, ref: 'Project' }]
+})
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
