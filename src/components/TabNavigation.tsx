3c98211d96c4f8330491333907f62db9b0c1ca2a
claireandj666 <claire.yandi.chen@gmail.com>
2024-12-10 16:16:56 -0800

    connect to user database to get projects detals and build dashboard page

---
+++ src/components/TabNavigation.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+interface TabNavigationProps {
+  activeTab: string;
+  handleTabChange: (activeTab: string) => void;
+}
+
+const TabNavigation: React.FC<TabNavigationProps> = ({
+  activeTab,
+  handleTabChange,
+}) => {
+  return (
+    <section id='tabs'>
+      <button
+        className={activeTab === 'Full Tree' ? 'active' : ''}
+        onClick={() => handleTabChange('Full Tree')}
+      >
+        Full Tree
+      </button>
+      <button
+        className={activeTab === 'Tab Index' ? 'active' : ''}
+        onClick={() => handleTabChange('Tab Index')}
+      >
+        Tab Index
+      </button>
+      <button
+        className={activeTab === 'Headers' ? 'active' : ''}
+        onClick={() => handleTabChange('Headers')}
+      >
+        Headers
+      </button>
+      <button
+        className={activeTab === 'Links' ? 'active' : ''}
+        onClick={() => handleTabChange('Links')}
+      >
+        Links
+      </button>
+      <button
+        className={activeTab === 'Non Contextual Links' ? 'active' : ''}
+        onClick={() => handleTabChange('Non Contextual Links')}
+      >
+        Non Semantic Links
+      </button>
+      <button
+        className={activeTab === 'Skip Link' ? 'active' : ''}
+        onClick={() => handleTabChange('Skip Link')}
+      >
+        Skip Link
+      </button>
+    </section>
+  );
+};
+
+export default TabNavigation;
--- src/pages/MainDashboard.tsx
+++ src/pages/MainDashboard.tsx
@@ -1,13 +1,21 @@
 // import * as React from 'react';
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ProjectForm from '../components/ProjectForm';
+import TabNavigation from '../components/TabNavigation';
 
 function MainDashboard() {
+
+	const [activeTab, setActiveTab] = useState('');
+
+	const handleclick = (e: string) => {
+		setActiveTab(e);
+	}
 
 	return (
 		<>
 			<h2>Accessibility Tree</h2>
 			<ProjectForm></ProjectForm>
+			<TabNavigation activeTab={activeTab} handleTabChange={handleclick} />
 		</>
 	)
 }
