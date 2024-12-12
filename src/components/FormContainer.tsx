f2f0fdd3d9cc855c493caefac45ac1605177d985
jamagachi <tamika.blakey@gmail.com>
2024-12-12 11:22:15 -0700

    style: add copies of mouse hover styling and adapt for keyboard focus (WIP)

--- src/components/FormContainer.tsx
+++ src/components/FormContainer.tsx
@@ -1,22 +1,35 @@
 import { useState } from 'react';
-import PageForm from '../components/PageForm'
-import ProjectForm from '../components/ProjectForm'
+import PageForm from '../components/PageForm';
+import ProjectForm from '../components/ProjectForm';
 import { FormContainerProps, Project } from '../types';
 
+function FormContainer({
+  pageResults,
+  setPageResults,
+  userInfo,
+}: FormContainerProps) {
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  // project state
+  // update project state handler -> passed to projectform
+  // from project state fetch data from user
+  // pass pages setPageResults to PageForm so that it can update pageResults
 
-function FormContainer({ pageResults,setPageResults, userInfo }:FormContainerProps) {
-    const [selectedProject, setSelectedProject] = useState<Project | null>(null);
-    // project state
-    // update project state handler -> passed to projectform
-    // from project state fetch data from user
-    // pass pages setPageResults to PageForm so that it can update pageResults
-
-	return (
-		<>
-            <ProjectForm userInfo={userInfo} setSelectedProject = {setSelectedProject} selectedProject = {selectedProject}/>
-            {selectedProject && <PageForm setPageResults = {setPageResults} selectedProject = {selectedProject} pageResults = {pageResults} />}
-		</>
-	)
+  return (
+    <>
+      <ProjectForm
+        userInfo={userInfo}
+        setSelectedProject={setSelectedProject}
+        selectedProject={selectedProject}
+      />
+      {selectedProject && (
+        <PageForm
+          setPageResults={setPageResults}
+          selectedProject={selectedProject}
+          pageResults={pageResults}
+        />
+      )}
+    </>
+  );
 }
 
 export default FormContainer;
