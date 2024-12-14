7ac7228c28fc1b1b6ddb7c0708f3ede76aa2e4fc
Jeetp45 <141786979+Jeetp45@users.noreply.github.com>
2024-12-14 13:06:32 -0500

    feat: refactor oAuth,cookie, and session controllers with TypeScript and comments

--- server/controllers/sessionController.ts
+++ server/controllers/sessionController.ts
@@ -1,36 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
 import SessionModel from '../models/sessionModel';
+import { sessionController } from '../type';
 
-const sessionController = {
-  startSession: async (req: Request, res: Response, next: NextFunction) => {
-    console.log('got to session controller');
-    try {
-      if (!res.locals.ssid) {
-        return next({
-          log: 'Error in sessionController.startSession: Missing ssid in res.locals',
-          status: 400,
-          message: { err: 'Session ID is required to start a session' },
-        });
-      }
-      const result = await SessionModel.findOne({ cookieId: res.locals.ssid });
-      if (!result) {
-        const session = await SessionModel.create({
-          cookieId: res.locals.ssid,
-        });
-        console.log('Session was created successfully:', session);
-        return next();
-      } else {
-        console.log('Session already exists:', result);
-        return next();
-      }
-    } catch {
+const SessionController = {} as sessionController;
+
+SessionController.startSession = async (req, res, next) => {
+  try {
+    if (!res.locals.ssid) {
       return next({
-        log: 'Error in sessionController.startSession: Failed to start session',
-        status: 500,
-        message: { err: 'An error occurred while starting the session' },
+        log: 'Error in sessionController.startSession: Missing ssid in res.locals',
+        status: 400,
+        message: { err: 'Session ID is required to start a session' },
       });
     }
-  },
+    //look for session in the database (based on res.locals.ssid)
+    const result = await SessionModel.findOne({ cookieId: res.locals.ssid });
+
+    if (!result) {
+      //create session if one doesn't already exist
+      const session = await SessionModel.create({
+        cookieId: res.locals.ssid,
+      });
+      console.log('Session was created successfully:', session);
+      return next();
+    } else {
+      console.log('Session already exists:', result);
+      return next();
+    }
+  } catch {
+    return next({
+      log: 'Error in sessionController.startSession: Failed to start session',
+      status: 500,
+      message: { err: 'An error occurred while starting the session' },
+    });
+  }
 };
 
-export default sessionController;
+export default SessionController;
--- server/type.d.ts
+++ server/type.d.ts
@@ -13,17 +13,33 @@ export type userController = {
   postUser: (req: Request, res: Response, next: NextFunction) => void;
   updateUser: (req: Request, res: Response, next: NextFunction) => void;
   deleteUser: (req: Request, res: Response, next: NextFunction) => void;
-}
+};
 
 export type projectController = {
   getProject: (req: Request, res: Response, next: NextFunction) => void;
   postProject: (req: Request, res: Response, next: NextFunction) => void;
   updateProject: (req: Request, res: Response, next: NextFunction) => void;
   deleteProject: (req: Request, res: Response, next: NextFunction) => void;
-}
+};
 
 export type pageController = {
   getPage: (req: Request, res: Response, next: NextFunction) => void;
   postPage: (req: Request, res: Response, next: NextFunction) => void;
   deletePage: (req: Request, res: Response, next: NextFunction) => void;
-}
+};
+
+export type oauthController = {
+  getTemporaryCode: (req: Request, res: Response, next: NextFunction) => void;
+  requestToken: (req: Request, res: Response, next: NextFunction) => void;
+  getUserData: (req: Request, res: Response, next: NextFunction) => void;
+  saveUser: (req: Request, res: Response, next: NextFunction) => void;
+  checkStatus: (req: Request, res: Response, next: NextFunction) => void;
+};
+
+export type cookieController = {
+  setSSIDCookie: (req: Request, res: Response, next: NextFunction) => void;
+};
+
+export type sessionController = {
+  startSession: (req: Request, res: Response, next: NextFunction) => void;
+};
