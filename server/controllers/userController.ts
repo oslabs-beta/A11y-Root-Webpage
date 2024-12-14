8f933823087c6d9c4a85996e37b678367fbaaa5e
warren-cutler <warrenecutler@gmail.com>
2024-12-14 13:02:59 -0500

    refactor: userController from thenables to try/catch syntax. Consistent error handling and null result handling

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -6,29 +6,26 @@ const UserController = {} as userController;
 UserController.getUser = async (req, res, next) => {
   //user to find: _id included as request param
   const userId = req.params.userId;
-
-  UserModel.findById({ _id: userId })
-    .then((user) => {
-      //if no user found --> DB returns null
-      if (!user) {
-        return res.status(204).send('No user found.');
-      }
-      res.locals.user = user;
-      return next();
-    })
-    .catch((err) => {
-      return next({
-        log: `Error in UserController.getUser: ERROR: ${err}`,
-        message: { err: 'An error occurred while retrieving user info.' },
-        status: 500,
-      });
+  try {
+    const user = await UserModel.findById({ _id: userId });
+    //if no user found --> DB returns null
+    if (!user) {
+      return res.status(204).send('No user found.');
+    }
+    res.locals.user = user;
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in UserController.getUser: ERROR: ${err}`,
+      message: { err: 'An error occurred while retrieving user info.' },
+      status: 500,
     });
+  }
 };
 
 UserController.postUser = async (req, res, next) => {
   //parse request body
   const { githubId, username, profileUrl, avatarUrl } = req.body;
-
   //check if required information exists in req.body
   if (!githubId || !username || !profileUrl || !avatarUrl) {
     return next({
@@ -47,19 +44,18 @@ UserController.postUser = async (req, res, next) => {
     projects: [],
   };
 
-  //create newUser in DB.
-  UserModel.create(newUser)
-    .then((user) => {
-      res.locals.user = user;
-      return next();
-    })
-    .catch((err) => {
-      return next({
-        log: `Error in UserController.postUser: ERROR: ${err}`,
-        message: { err: 'An error occurred creating new User.' },
-        status: 500,
-      });
+  try {
+    //create newUser in DB.
+    const user = await UserModel.create(newUser);
+    res.locals.user = user;
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in UserController.postUser: ERROR: ${err}`,
+      message: { err: 'An error occurred creating new User.' },
+      status: 500,
     });
+  }
 };
 
 UserController.updateUser = async (req, res, next) => {
@@ -67,6 +63,7 @@ UserController.updateUser = async (req, res, next) => {
   const { githubId, projectId } = req.body;
 
   try {
+    //update list of user's projects for existing user
     const user = await UserModel.findOneAndUpdate(
       { githubId },
       { $push: { projects: projectId } },
@@ -75,23 +72,17 @@ UserController.updateUser = async (req, res, next) => {
 
     //if no user found to update --> DB returns null
     if (!user) {
-      return next({
-        log: `user not found`,
-        message: `user not found in UserController.updateUser`,
-        status: 204,
-      });
+      return res.status(204).send('No user found.');
     }
 
     res.locals.user = user;
     return next();
-  } catch (error) {
-    const err = {
-      log:
-        `Express error handler caught error in updateUser middleware` + error,
+  } catch (err) {
+    return next({
+      log: `Error in UserController.updateUser: ERROR: ${err}`,
       message: { err: 'An error occurred while updating the user.' },
       status: 500,
-    };
-    return next(err);
+    });
   }
 };
 
@@ -99,23 +90,22 @@ UserController.deleteUser = async (req, res, next) => {
   //user to delete: githubId included as request param
   const githubId = req.params.githubId;
 
-  UserModel.findOneAndDelete({ githubId })
-    .then((user) => {
-      //if no user found --> DB returns null
-      if (!user) {
-        return res.status(204).send('No user found.');
-      }
+  try {
+    const user = await UserModel.findOneAndDelete({ githubId });
+    //if no user found --> DB returns null
+    if (!user) {
+      return res.status(204).send('No user found.');
+    }
 
-      res.locals.user = user;
-      return next();
-    })
-    .catch((err) => {
-      return next({
-        log: `Error in UserController.deleteUser: ERROR: ${err}`,
-        message: { err: 'An error occurred while trying to delete the user.' },
-        status: 500,
-      });
+    res.locals.user = user;
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in UserController.deleteUser: ERROR: ${err}`,
+      message: { err: 'An error occurred while trying to delete the user.' },
+      status: 500,
     });
+  }
 };
 
 UserController.fullUserDetails = async (req, res, next) => {
