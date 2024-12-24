324636cc56c59e6864a7de89ca264becc9003da0
Ian Buchanan <get.ian.buchanan@gmail.com>
2024-12-24 14:03:18 -0500

    add logs for extensionRoute

--- server/routes/extensionRoute.ts
+++ server/routes/extensionRoute.ts
@@ -39,6 +39,10 @@ extensionRoute.get(
   oAuthController.saveUser,
   sessionController.startSession,
   (req: Request, res: Response) => {
+    if (!req.query.code) {
+      console.log('No code provided');
+      return res.status(400).json({ error: 'No code provided' });
+    }
     return res.status(200).json(res.locals.user);
   }
 );
