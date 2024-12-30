2896ee4dfe2af3079ad62e5c2824e04518eeba61
Ian Buchanan <get.ian.buchanan@gmail.com>
2024-12-30 15:13:01 -0500

    feat: add link to vscode extension

--- src/components/BtnDownload.d.ts
+++ src/components/BtnDownload.d.ts
@@ -1,7 +1,1 @@
-interface BtnDownloadProps {
-  handleDownload: () => void;
-}
-declare const BtnDownload: ({
-  handleDownload,
-}: BtnDownloadProps) => import('react/jsx-runtime').JSX.Element;
-export default BtnDownload;
+
