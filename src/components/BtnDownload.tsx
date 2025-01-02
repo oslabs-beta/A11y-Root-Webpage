c22387d379501402503f7963f3b2e6b1740bb25a
jamagachi <tamika.blakey@gmail.com>
2025-01-02 11:27:17 -0700

    feat: change BtnDownload component from a react-aria-component Link to a react-aria-component Button

--- src/components/BtnDownload.tsx
+++ src/components/BtnDownload.tsx
@@ -2,15 +2,23 @@ import { Button } from 'react-aria-components';
 
 // interface handles the prop type issue in params
 
+const handleDownloadClick = () => {
+  window.location.href =
+    'https://github.com/oslabs-beta/A11y-Root-Extension/releases/download/test/a11y-root-extension-0.0.1.vsix';
+};
+
 const BtnDownload = () => {
   return (
     <div className='btn-download'>
-      {/* Using Link component instead of Button */}
-      <Button href='https://github.com/oslabs-beta/A11y-Root-Extension/releases/download/test/a11y-root-extension-0.0.1.vsix'>
-        Download VS Code Extension
-      </Button>
+      {/* temporarily changed Link to Button to avoid errors */}
+      <Button onPress={handleDownloadClick}>Download VS Code Extension</Button>
     </div>
   );
 };
 
 export default BtnDownload;
+
+// {/* Using Link component instead of Button */}
+// <Link href='https://github.com/oslabs-beta/A11y-Root-Extension/releases/download/test/a11y-root-extension-0.0.1.vsix'>
+// Download VS Code Extension
+// </Link>
