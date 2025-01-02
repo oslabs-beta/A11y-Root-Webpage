1737c54eb171fd3483de4c32cb85d866fad6b331
jamagachi <tamika.blakey@gmail.com>
2025-01-02 09:28:42 -0700

    chore: add image of quote with web a11y icon to assets

--- src/pages/NewHome.tsx
+++ src/pages/NewHome.tsx
@@ -7,28 +7,38 @@ const NewHome = () => {
   return (
     <main className='newhome'>
       <img id='sect-0' src={roots} alt='' />
+
       <section id='sect-1' className='home-intro'>
         <p>
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Nulla gravida
-          netus nulla adipiscing, consequat pellentesque finibus. Hendrerit
-          quisque et semper, duis porttitor et. Rutrum amet primis condimentum
-          tempus aenean etiam ut, interdum magna.
+          Transform your web projects with real-time insights into WCAG A-level
+          compliance. Our tool simplifies accessibility debugging for
+          developers, ensuring no user is left behind.
         </p>
       </section>
+
       <section id='sect-2' className='home-quote'>
         <p id='s2-item-1'>"BUILD APPLICATIONS ROOTED IN ACCESSIBILITY"</p>
         <img id='s2-item-2' src={webA11yIcon} alt='' />
       </section>
+
       <section id='sect-3' className='home-a11yroot'>
         <img id='s3-item-1' src={developers} alt='' />
-        <h2 id='s3-item-2'>What Is A11y Root</h2>
-        <p id='s3-item-3'>
-          A11y (pronounced ‘Accessibility’ or ‘Aye-eleven-why’) Lorem ipsum odor
-          amet, consectetuer adipiscing elit. Nulla gravida netus nulla
-          adipiscing, consequat pellentesque finibus.
-        </p>
+        <div id='sect-3-details'>
+          <h2 id='s3-item-2'>What Is A11y Root</h2>
+          <p id='s3-item-3'>
+            A11y (pronounced ‘Accessibility’ or ‘Aye-eleven-why’) Lorem ipsum
+            odor amet, consectetuer adipiscing elit. Nulla gravida netus nulla
+            adipiscing, consequat pellentesque finibus.
+          </p>
+        </div>
+      </section>
+
+      <section id='sect-4' className='home-quote'>
         <h2>Assistive Technologies Depend On Web Accessibility!</h2>
-        <section id='sect-4' className='home-weba11y'>
+      </section>
+
+      <section id='sect-5' className='home-weba11y'>
+        <div id='sect-5-details'>
           <h2>What Is Web Accessibility?</h2>
           <p>
             Assistive technologies are hindered by poorly constructed
@@ -38,8 +48,8 @@ const NewHome = () => {
             then struggle to navigate the website and are given limited access
             to its features.
           </p>
-          <img src={user} alt='' />
-        </section>
+        </div>
+        <img src={user} alt='' />
       </section>
     </main>
   );
