eac20f0630caabfa5a76d4d9a99a1896114d227d
jamagachi <tamika.blakey@gmail.com>
2025-01-01 23:49:40 -0700

    feat: import recently added images to new home page component, NewHome.tsx

--- src/pages/NewHome.tsx
+++ src/pages/NewHome.tsx
@@ -1,7 +1,12 @@
+import roots from '../assets/roots.svg';
+import weba11yIcon from '../assets/icon-weba11y.svg';
+import developers from '../assets/developers.svg';
+import user from '../assets/user.svg';
+
 const NewHome = () => {
   return (
     <main className='newhome'>
-      <h1>Home 2</h1>
+      <h2>Home 2</h2>
     </main>
   );
 };
